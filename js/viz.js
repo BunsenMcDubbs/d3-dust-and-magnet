ac30a288ca3ce3a4080d7de55d984941e5ab38ee
Andrew Dai <theman@andrewdai.co>
2015-09-02 23:13:39 -0400

    Fix physics, it mostly works

--- js/viz.js
+++ js/viz.js
@@ -5,18 +5,18 @@ function render(id, fData) {
     /**
      * General info about the window and the visualization
      */
-    var panelHeight = 50
-    var width = $(window).width() - 50
-    var height = $(window).height()
-    var padding = 10
-    var dustRadius = 5
-    var magnetRadius = 20
+    var panelHeight = 50;
+    var width = $(window).width() - 50;
+    var height = $(window).height();
+    var padding = 10;
+    var dustRadius = 5;
+    var magnetRadius = 20;
 
     // get properties of the data
-    var props = new Array()
-    var p = fData[0]
+    var props = [];
+    var p = fData[0];
     for (var key in p) {
-        props.push(key)
+        props.push(key);
     }
 
     var keys = [
@@ -26,18 +26,18 @@ function render(id, fData) {
         {name: 'HP'},
         {name: 'CityMPG'},
         {name: 'HwyMPG'}
-        ]
+    ];
 
     // keep track of the magnets that are active (clicked)
-    var activeMagnets = []
+    var activeMagnets = [];
     keys.forEach(function(d) {
         //activeMagnets.push({name: d.name, active: false})
         // calculate the min and max for each
-        d.min = d3.min(fData, function(val) { return val[d.name]; })
-        d.max = d3.max(fData, function(val) { return val[d.name]; })
-        d.active = false
-        d.fill = 'black'
-    })
+        d.min = d3.min(fData, function(val) { return val[d.name]; });
+        d.max = d3.max(fData, function(val) { return val[d.name]; });
+        d.active = false;
+        d.fill = 'black';
+    });
 
     /**
      * Funtion to handle tooltips over the dust
@@ -46,73 +46,59 @@ function render(id, fData) {
                 .attr('class', 'd3-tip')
                 .offset([-10, 0])
                 .html(function(d) {
-                    console.log(d);
                     return "<strong>"+ d.name +"</strong>";
-                })
+                });
 
     var drag = d3.behavior.drag()
                 .origin(function(d) { return d; })
-                .on("drag", dragmove)
-                .on("dragend", updateDust)
+                .on("drag", dragmove);
+                // .on("drag", updateDust);
 
     function dragmove(d) {
-        console.log('here');
-        console.log(d);
-        console.log(d3.event.x);
         d3.select(this)
             .attr("cx", d.x = Math.max(magnetRadius, Math.min(width - magnetRadius, d3.event.x)))
             .attr("cy", d.y = Math.max(magnetRadius, Math.min(height - magnetRadius, d3.event.y)));
     }
 
+    d3.timer(updateDust);
+
     function updateDust() {
         d3.selectAll('.dust-circle')
-            .transition()
-            .duration(500)
-            // .delay(function(d, i) {
-            //     return i * 10
-            // })
-            .ease('bounce')
+            // .transition()
+            // .duration(500)
+            // // .delay(function(d, i) {
+            // //     return i * 10
+            // // })
+            // .ease('bounce')
             .attr('cx', function(d) {
                 // current loc
-                var x = d.x
+                var dx = 0;
                 keys.forEach(function(key) {
-                    if (key.active == true) {
-                        var val = key.name
-                        var dustVal = d[val]
-                        // get the difference in distance
-                        var deltaX = key.x - x
-                        // get the force scalar
-                        var scale = d3.scale.linear().domain([key.min, key.max]).range([0.1, 0.9])
-                        var force = scale(dustVal)
-                        x += deltaX * force
+                    if (key.active === true) {
+                        dx += d[key.name] * (key.x - d.x) / 50;
                     }
-                })
-                return x
+                });
+                d.x += dx;
+                return d.x;
             })
             .attr('cy', function(d) {
                 // current loc
-                var y = d.y
+                var dy = 0;
                 keys.forEach(function(key) {
-                    if (key.active == true) {
-                        var val = key.name
-                        var dustVal = d[val]
-                        // get the difference in distance
-                        var deltaY = key.y - y
-                        // get the force scalar
-                        var scale = d3.scale.linear().domain([key.min, key.max]).range([0.1, 0.9])
-                        var force = scale(dustVal)
-                        y += deltaY * force
+                    if (key.active === true) {
+                        dy += d[key.name] * (key.y - d.y) / 50;
                     }
-                })
-                return y
-            })
+                });
+                d.y += dy;
+                return d.y;
+            });
     }
 
     function updateMagnets() {
         d3.selectAll('.magnet-circle')
             .attr('fill', function(d) {
-                return d.fill
-            })
+                return d.fill;
+            });
     }
 
     /**************************************************************************/
@@ -131,43 +117,45 @@ function render(id, fData) {
                         .enter()
                         .append('g')
                         .attr('class', 'magnet-group')
-                        .call(tip)
+                        .call(tip);
 
     //magnets.call(tip)
     // create the magnet circles
     magnets.append('circle')
             .attr('class', 'magnet-circle')
             .attr('cx', function(d, i) {
-                d.x = i * 2 * magnetRadius + i * padding + padding + 40
-                return d.x
+                d.x = i * 2 * magnetRadius + i * padding + padding + 40;
+                return d.x;
             })
             .attr('cy', function(d, i) {
-                d.y = height - (magnetRadius + padding)
-                return d.y
+                d.y = height - (magnetRadius + padding);
+                return d.y;
             })
             .attr('r', function(d, i) {
-                return magnetRadius
+                return magnetRadius;
             })
             .attr('fill', function(d) {
-                return d.fill
+                return d.fill;
             })
             .call(drag)
             .on('mouseover', tip.show)
             .on('mouseout', tip.hide)
             .on('dblclick', function(d) {
-                d.active = !d.active
-                if (d.active == true)
-                    d.fill = 'red'
-                else d.fill = 'black'
-                updateDust()
-                updateMagnets()
-            })
+                d.active = !d.active;
+                if (d.active === true) {
+                    d.fill = 'red';
+                } else {
+                    d.fill = 'black';
+                }
+                updateDust();
+                updateMagnets();
+            });
     var dust = svg.selectAll('.dust-group')
                         .data(fData)
                         .enter()
                         .append('g')
                         .attr('class', 'dust-group');
-    dust.call(tip)
+    dust.call(tip);
     // add in the dust
     dust.append('circle')
         .attr('class', 'dust-circle')
@@ -184,5 +172,5 @@ function render(id, fData) {
         .attr('r', 5)
         .attr('fill', 'blue')
         .on('mouseover', tip.show)
-        .on('mouseout', tip.hide)
+        .on('mouseout', tip.hide);
 }
